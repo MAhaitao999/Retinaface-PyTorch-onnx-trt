3bcc00190c771992073abb8aea5deba62e760629
Henry Ma <769413715@qq.com>
2021-02-20 18:12:21 +0800

    add train

---
+++ nets/retinaface.py
@@ -0,0 +1,142 @@
+from collections import OrderedDict
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision.models._utils import IntermediateLayerGetter
+from torchvision import models
+
+from nets.layers import FPN, SSH
+from nets.mobilenet025 import MobileNetV1
+
+
+#---------------------------------------------------#
+#   种类预测（是否包含人脸）
+#---------------------------------------------------#
+class ClassHead(nn.Module):
+    def __init__(self, in_channels=512, num_anchors=2):
+        super(ClassHead, self).__init__()
+        self.conv1x1 = nn.Conv2d(in_channels, num_anchors*2, kernel_size=(1, 1), stride=1, padding=0)
+
+    def forward(self, x):
+        out = self.conv1x1(x)
+        out = out.permute(0, 2, 3, 1).contiguous()
+
+        return out.view(out.shape[0], -1, 2)
+
+
+#---------------------------------------------------#
+#   预测框预测
+#---------------------------------------------------#
+class BboxHead(nn.Module):
+    def __init__(self, in_channels=512, num_anchors=2):
+        super(BboxHead, self).__init__()
+        self.conv1x1 = nn.Conv2d(in_channels, num_anchors*4, kernel_size=(1, 1), stride=1, padding=0)
+
+    def forward(self, x):
+        out = self.conv1x1(x)
+        out = out.permute(0, 2, 3, 1).contiguous()
+
+        return out.view(out.shape[0], -1, 4)
+
+
+#---------------------------------------------------#
+#   人脸关键点预测
+#---------------------------------------------------#
+class LandmarkHead(nn.Module):
+    def __init__(self, in_channels=512, num_anchors=2):
+        super(LandmarkHead, self).__init__()
+        self.conv1x1 = nn.Conv2d(in_channels, num_anchors*10, kernel_size=(1, 1), stride=1, padding=0)
+
+    def forward(self, x):
+        out = self.conv1x1(x)
+        out = out.permute(0, 2, 3, 1).contiguous()
+
+        return out.view(out.shape[0], -1, 10)
+
+
+class RetinaFace(nn.Module):
+    def __init__(self, cfg=None, pretrained=False, mode='train'):
+        super(RetinaFace, self).__init__()
+        backbone = None
+        if cfg['name'] == 'mobilenet0.25':
+            backbone = MobileNetV1()
+            if pretrained:
+                checkpoint = torch.load('./model_data/mobilenetV1X0.25_pretrain.tar', map_location=torch.device('cpu'))
+                new_state_dict = OrderedDict()
+                for k, v in checkpoint['state_dict'].items():
+                    name = k[7:]
+                    new_state_dict[name] = v
+                backbone.load_state_dict(new_state_dict)
+        elif cfg['name'] == 'Resnet50':
+            backbone = models.resnet50(pretrained=pretrained)
+
+        self.body = IntermediateLayerGetter(backbone, cfg['return_layers'])
+
+        # 获得每个初步有效特征层的通道数
+        in_channels_list = [cfg['in_channel'] * 2, cfg['in_channel'] * 4, cfg['in_channel'] * 8]
+        self.fpn = FPN(in_channels_list, cfg['out_channel'])
+        # 利用ssh模块提高模型感受野
+        self.ssh1 = SSH(cfg['out_channel'], cfg['out_channel'])
+        self.ssh2 = SSH(cfg['out_channel'], cfg['out_channel'])
+        self.ssh3 = SSH(cfg['out_channel'], cfg['out_channel'])
+
+        self.ClassHead = self._make_class_head(fpn_num=3, in_channels=cfg['out_channel'])
+        self.BboxHead = self._make_bbox_head(fpn_num=3, in_channels=cfg['out_channel'])
+        self.LandmarkHead = self._make_landmark_head(fpn_num=3, in_channels=cfg['out_channel'])
+
+    @staticmethod
+    def _make_class_head(fpn_num=3, in_channels=64, anchor_num=2):
+        classhead = nn.ModuleList()
+        for i in range(fpn_num):
+            classhead.append(ClassHead(in_channels, anchor_num))
+        return classhead
+
+    @staticmethod
+    def _make_bbox_head(fpn_num=3, in_channels=64, anchor_num=2):
+        bboxhead = nn.ModuleList()
+        for i in range(fpn_num):
+            bboxhead.append(BboxHead(in_channels, anchor_num))
+        return bboxhead
+
+    @staticmethod
+    def _make_landmark_head(fpn_num=3, in_channels=64, anchor_num=2):
+        landmarkhead = nn.ModuleList()
+        for i in range(fpn_num):
+            landmarkhead.append(LandmarkHead(in_channels, anchor_num))
+        return landmarkhead
+
+    def forward(self, inputs):
+        #-------------------------------------------#
+        #   获得三个shape的有效特征层
+        #   分别是C3 80, 80, 64
+        #        C4 40, 40, 128
+        #        C5 20, 20, 256
+        #-------------------------------------------#
+        out = self.body.forward(inputs)
+
+        #-------------------------------------------#
+        #   获得三个shape的有效特征层
+        #   分别是output1  80, 80, 64
+        #        output2  40, 40, 64
+        #        output3  20, 20, 64
+        #-------------------------------------------#
+        fpn = self.fpn.forward(out)
+
+        feature1 = self.ssh1(fpn[0])
+        feature2 = self.ssh2(fpn[1])
+        feature3 = self.ssh3(fpn[2])
+        features = [feature1, feature2, feature3]
+
+        # -------------------------------------------#
+        #   将所有结果进行堆叠
+        # -------------------------------------------#
+        bbox_regressions = torch.cat([self.BboxHead[i](feature) for i, feature in enumerate(features)], dim=1)
+        classifications = torch.cat([self.ClassHead[i](feature) for i, feature in enumerate(features)], dim=1)
+        ldm_regressions = torch.cat([self.LandmarkHead[i](feature) for i, feature in enumerate(features)], dim=1)
+
+        if self.mode == 'train':
+            output = (bbox_regressions, classifications, ldm_regressions)
+        else:
+            output = (bbox_regressions, F.softmax(classifications, dim=-1), ldm_regressions)
+        return output
---
+++ train.py
@@ -0,0 +1,202 @@
+import os
+import math
+import time
+import argparse
+import datetime
+
+import numpy as np
+import torch
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+from nets.retinaface import RetinaFace
+from nets.retinaface_training import (DataGenerator, MultiBoxLoss,
+                                      detection_collate)
+from utils.anchors import Anchors
+from utils.config import cfg_mnet, cfg_re50
+
+
+def get_lr(optimizer):
+    for param_group in optimizer.param_groups:
+        return param_group['lr']
+
+
+def fit_one_epoch(model, net, criterion, optimizer, epoch, epoch_size, gen, Epoch, anchors, cfg, cuda):
+    total_r_loss = 0
+    total_c_loss = 0
+    total_landmark_loss = 0
+
+    with tqdm(total=epoch_size, desc=f'Epoch {epoch + 1}/{Epoch}', postfix=dict, mininterval=0.3) as pbar:
+        for iteration, batch in enumerate(gen):
+            if iteration >= epoch_size:
+                break
+            images, targets = batch[0], batch[1]
+            if len(images) == 0:
+                continue
+
+            with torch.no_grad():
+                if cuda:
+                    images = Variable(torch.from_numpy(images).type(torch.FloatTensor)).cuda()
+                    targets = [Variable(torch.from_numpy(ann).type(torch.FloatTensor)).cuda() for ann in targets]
+                else:
+                    images = Variable(torch.from_numpy(images).type(torch.FloatTensor))
+                    targets = [Variable(torch.from_numpy(ann).type(torch.FloatTensor)) for ann in targets]
+            #----------------------#
+            #   清零梯度
+            #----------------------#
+            optimizer.zero_grad()
+            #----------------------#
+            #   前向传播
+            #----------------------#
+            out = net(images)
+            # ----------------------#
+            #   计算损失
+            # ----------------------#
+            r_loss, c_loss, landm_loss = criterion(out, anchors, targets)
+            loss = cfg['loc_weight'] * r_loss + c_loss + landm_loss
+
+            loss.backward()
+            optimizer.step()
+
+            total_c_loss += c_loss.item()
+            total_r_loss += cfg['loc_weight'] * r_loss.item()
+            total_landmark_loss += landm_loss.item()
+
+            pbar.set_postfix(**{'Conf Loss': total_c_loss / (iteration + 1),
+                                'Regression Loss': total_r_loss / (iteration + 1),
+                                'LandMark Loss': total_landmark_loss / (iteration + 1),
+                                'lr': get_lr(optimizer)})
+            pbar.update(1)
+
+    print('Saving state, iter:', str(epoch + 1))
+    torch.save(model.state_dict(), 'logs/Epoch%d-Total_Loss%.4f.pth' % (
+        (epoch + 1), (total_c_loss + total_r_loss + total_landmark_loss) / (epoch_size + 1)))
+    return
+
+
+if __name__ == '__main__':
+    num_classes = 2
+    #-------------------------------#
+    #   是否使用Cuda
+    #   没有GPU可以设置成False
+    #-------------------------------#
+    Cuda = True
+    #--------------------------------#
+    #   获得训练用的人脸标签与坐标
+    #--------------------------------#
+    training_dataset_path = './data/widerface/train/label.txt'
+    #-------------------------------#
+    #   主干特征提取网络的选择
+    #   mobilenet或者resnet50
+    #-------------------------------#
+    backbone = "mobilenet"
+    #-------------------------------#
+    #   是否使用主干特征提取网络
+    #   的预训练权重
+    #-------------------------------#
+    pretrained = False
+
+    if backbone == 'mobilenet':
+        cfg = cfg_mnet
+    elif backbone == 'resnet50':
+        cfg = cfg_re50
+    else:
+        raise ValueError('Unsupported backbone - `{}`, Use mobilenet, resnet50.'.format(backbone))
+
+    img_dim = cfg['train_image_size']
+    #-------------------------------#
+    #   获得先验框anchors
+    #-------------------------------#
+    anchors = Anchors(cfg, image_size=(img_dim, img_dim)).get_anchors()
+    if Cuda:
+        anchors = anchors.cuda()
+
+    model = RetinaFace(cfg=cfg, pretrained=pretrained).train()
+    #-------------------------------------------#
+    #   权值文件的下载请看README
+    #   权值和主干特征提取网络一定要对应
+    #-------------------------------------------#
+    model_path = "model_data/Retinaface_mobilenet0.25.pth"
+    print('Loading weights into state dict...')
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model_dict = model.state_dict()
+    pretrained_dict = torch.load(model_path, map_location=device)
+    pretrained_dict = {k: v for k, v in pretrained_dict.items() if np.shape(model_dict[k]) == np.shape(v)}
+    model_dict.update(pretrained_dict)
+    model.load_state_dict(model_dict)
+    print('Finished!')
+
+    net = model
+    if Cuda:
+        net = torch.nn.DataParallel(model)
+        cudnn.benchmark = True
+        net = net.cuda()
+
+    criterion = MultiBoxLoss(num_classes, 0.35, 7, Cuda)
+
+    #------------------------------------------------------#
+    #   主干特征提取网络特征通用，冻结训练可以加快训练速度
+    #   也可以在训练初期防止权值被破坏。
+    #   Init_Epoch为起始世代
+    #   Freeze_Epoch为冻结训练的世代
+    #   Epoch总训练世代
+    #   提示OOM或者显存不足请调小Batch_size
+    #------------------------------------------------------#
+    if True:
+        #--------------------------------------------#
+        #   BATCH_SIZE不要太小，不然训练效果很差
+        #--------------------------------------------#
+        lr = 1e-3
+        Batch_size = 8
+        Init_Epoch = 0
+        Freeze_Epoch = 50
+
+        optimizer = optim.Adam(net.parameters(), lr, weight_decay=5e-4)
+        lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.92)
+
+        train_dataset = DataGenerator(training_dataset_path, img_dim)
+        gen = DataLoader(train_dataset, shuffle=True, batch_size=Batch_size, num_workers=4, pin_memory=True,
+                         drop_last=True, collate_fn=detection_collate)
+
+        epoch_size = train_dataset.get_len()//Batch_size
+        #------------------------------------#
+        #   冻结一定部分训练
+        #------------------------------------#
+        for param in model.body.parameters():
+            param.requires_grad = False
+
+        for epoch in range(Init_Epoch, Freeze_Epoch):
+            fit_one_epoch(model, net, criterion, optimizer, epoch, epoch_size, gen, Freeze_Epoch, anchors, cfg, Cuda)
+            lr_scheduler.step()
+
+    if True:
+        #--------------------------------------------#
+        #   BATCH_SIZE不要太小，不然训练效果很差
+        #--------------------------------------------#
+        lr = 1e-4
+        Batch_size = 8
+        Freeze_Epoch = 50
+        Unfreeze_Epoch = 100
+
+        optimizer = optim.Adam(net.parameters(), lr, weight_decay=5e-4)
+        lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.92)
+
+        train_dataset = DataGenerator(training_dataset_path, img_dim)
+        gen = DataLoader(train_dataset, shuffle=True, batch_size=Batch_size, num_workers=4, pin_memory=True,
+                         drop_last=True, collate_fn=detection_collate)
+
+        epoch_size = train_dataset.get_len() // Batch_size
+        # ------------------------------------#
+        #   解冻后训练
+        # ------------------------------------#
+        for param in model.body.parameters():
+            param.requires_grad = True
+
+        for epoch in range(Freeze_Epoch, Unfreeze_Epoch):
+            fit_one_epoch(model, net, criterion, optimizer, epoch, epoch_size, gen, Unfreeze_Epoch, anchors, cfg, Cuda)
+            lr_scheduler.step()
+
+
